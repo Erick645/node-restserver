5acc0bf08ae3561284120716d5dadf5914f7a67b
erick645 <erickrmz645@gmail.com>
2018-10-28 18:51:34 -0600

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,16 @@
 // Puerto
 //===================
 process.env.PORT = process.env.PORT || 3000;
+
+//===================
+// Entorno
+//===================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://cafe-user:cafe123456@ds143953.mlab.com:43953/cafe';
+}
+
+process.env.URLDB = urlDB;
