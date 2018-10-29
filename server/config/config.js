bb78b4b846ac7bbb8ef0d903e04f6dd0cac69cdd
erick645 <erickrmz645@gmail.com>
2018-10-28 19:16:12 -0600

    Corrección db connection

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-user:cafe123456@ds143953.mlab.com:43953/cafe';
+    urlDB = 'mongodb://cafe2:cafe123@ds143953.mlab.com:43953/cafe';
 }
 
 process.env.URLDB = urlDB;
